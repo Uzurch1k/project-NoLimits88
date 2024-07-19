24aa54981ea06b461864ea501dafc26039a82480
Voytovic <voytovic@gmail.com>
2024-07-20 00:51:57 +0300

    Update SignUpForm.jsx

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -5,7 +5,7 @@ import * as yup from 'yup';
 import axios from 'axios';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { useNavigate } from 'react-router-dom'; // Використовуємо useNavigate замість useHistory
+import { useNavigate } from 'react-router-dom';
 import css from './SignUpForm.module.scss';
 import icons from '../../img/icons/icons.svg';
 
