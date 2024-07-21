c49c6a982308acf07164f30fa98965f4a8783922
DianaKiriienko <diana.kirienko.homiak@gmail.com>
2024-07-21 17:07:01 +0300

    LogOutModal
    + змінила overflow-y

--- src/components/LogOutModal/LogOutModal.jsx
+++ src/components/LogOutModal/LogOutModal.jsx
@@ -1,13 +1,32 @@
 import css from './LogOutModal.module.scss';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import Modal from '../Modal/Modal';
+import { logOut } from '../../redux/auth/operations'
 
 const LogOutModal = ({ isOpen, onClose }) => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      await dispatch(logOut()).unwrap();
+    } catch (error) {
+      console.error('Logout failed', error);
+    } finally {
+      dispatch({ type: 'CLEAR_STORE' });
+      localStorage.clear();
+      navigate('/');
+    }
+  };
+
   return (
-    <div className={css.bloc}>
-      <Modal isOpen={isOpen} onClose={onClose}>
-        <h2>Log out</h2>
+      <Modal isOpen={isOpen} onClose={onClose} className={css.content}>
+        <h2 className={css.title}>Log out</h2>
+        <p className={css.quest}>Do you really want to leave?</p>
+        <button className={css.btnlogout} type='button' onClick={handleLogout}> Log Out </button>
+        <button className={css.btncancel} type='button' onClick={onClose}>Cancel</button>
       </Modal>
-    </div>
   );
 };
 
