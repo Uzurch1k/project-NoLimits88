364c62402396fe70840e79c0ba546ae9a5c031c3
Uzurch1k <galcenkovlad7@gmail.com>
2024-07-22 15:20:16 +0300

    fix

--- src/components/Layout/Section/Section.jsx
+++ src/components/Layout/Section/Section.jsx
@@ -3,9 +3,7 @@ import css from './Section.module.scss';
 const Section = ({ children }) => {
   return (
     <div className="container">
-      <div className={css.section}>
-        <div className={css.body}>{children}</div>
-      </div>
+      <div className={css.section}>{children}</div>
     </div>
   );
 };
