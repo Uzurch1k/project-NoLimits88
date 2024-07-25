1a05720e8e4aadf6a371ba13b30b7322d44fd972
Uzurch1k <galcenkovlad7@gmail.com>
2024-07-25 18:45:13 +0300

    fix

--- src/components/WaterItem/WaterItem.jsx
+++ src/components/WaterItem/WaterItem.jsx
@@ -3,22 +3,25 @@ import icons from '../../img/icons/symbol.svg';
 
 const WaterItem = () => {
   return (
-    <li className={css.water_item_content}>
-      <svg className={css.icon_water_glass} width="38" height="38">
-        <use href={`${icons}#icon-glass`}></use>
-      </svg>
-
-      <div className={css.water_info}>
-        <p className={css.water_amount}>Amount</p>
-        <p className={css.water_time}>Time</p>
+    <li className={css.waterItem}>
+      <div className={css.iconWaterWrapp}>
+        <svg className={css.iconWater} width="38" height="38">
+          <use href={`${icons}#icon-glass`}></use>
+        </svg>
       </div>
-      <div className={css.container_buttons}>
+
+      <div className={css.waterInfo}>
+        <p className={css.waterAmount}>250 ml</p>
+        <p className={css.waterTime}>7:00 AM</p>
+      </div>
+
+      <div className={css.buttonsBox}>
         <button
           className={css.editBtn}
           // onClick={openModalEdit}
           aria-label="Edit the entered amount of water"
         >
-          <svg className={css.icon_action} width="14" height="14">
+          <svg className={css.iconAction} width="14" height="14">
             <use href={`${icons}#icon-edit`}></use>
           </svg>
         </button>
@@ -27,7 +30,7 @@ const WaterItem = () => {
           // onClick={openModalDelete}
           aria-label="Delete the entered amount of water"
         >
-          <svg className={css.icon_action} width="14" height="14">
+          <svg className={css.iconAction} width="14" height="14">
             <use href={`${icons}#icon-trash`}></use>
           </svg>
         </button>
