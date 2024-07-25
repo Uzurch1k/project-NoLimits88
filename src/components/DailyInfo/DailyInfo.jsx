5329e270b85d8fe30a13bbea75715c5ca07df097
Uzurch1k <galcenkovlad7@gmail.com>
2024-07-25 16:18:11 +0300

    fix

--- src/components/DailyInfo/DailyInfo.jsx
+++ src/components/DailyInfo/DailyInfo.jsx
@@ -1,9 +1,20 @@
+import AddWaterBtn from '../AddWaterBtn/AddWaterBtn';
+import ChooseDate from '../ChooseDate/ChooseDate';
+import WaterList from '../WaterList/WaterList';
+
 import css from './DailyInfo.module.scss';
 
-const DailyInfo = () => {
+const DailyInfo = ({ openWaterModal }) => {
   return (
-    <div className={css.bloc}>
-      <h2>DailyInfo</h2>
+    <div className={css.dailyInfoSec}>
+      <div className={css.dailyWrapp}>
+        <ChooseDate />
+        <AddWaterBtn
+          waterClassBtn={css.waterClassBtn}
+          openWaterModal={openWaterModal}
+        />
+      </div>
+      <WaterList />
     </div>
   );
 };
