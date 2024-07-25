c0e0ee1402bcf256cbc5758478d49da27171d613
Marensfc <alexeymaren12@gmail.com>
2024-07-25 18:46:47 +0300

    calendar pagination styles

--- src/components/CalendarPagination/CalendarPagination.jsx
+++ src/components/CalendarPagination/CalendarPagination.jsx
@@ -1,9 +1,27 @@
 import css from './CalendarPagination.module.scss';
+import icons from '../../img/icons/icons.svg';
 
 const CalendarPagination = () => {
   return (
-    <div className={css.bloc}>
-      <h2>CalendarPagination</h2>
+    <div className={css.calendarPaginationWrapper}>
+      <div className={css.chooseMonthWrapper}>
+        <button>
+          <svg width="18" height="18" className={css.calendarArrowIcon}>
+            <use href={`${icons}#calendar-arrow-left`}></use>
+          </svg>
+        </button>
+        <p className={css.dateTitle}>Some date</p>
+        <button>
+          <svg width="18" height="18" className={css.calendarArrowIcon}>
+            <use href={`${icons}#calendar-arrow-right`}></use>
+          </svg>
+        </button>
+      </div>
+      <button className={css.statisticsButton}>
+        <svg width="20" height="20" className={css.iconStatistics}>
+          <use href={`${icons}#statistics`} />
+        </svg>
+      </button>
     </div>
   );
 };
