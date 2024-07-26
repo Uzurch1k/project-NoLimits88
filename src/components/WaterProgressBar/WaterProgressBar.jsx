3f905f627950f0b0c04f200cb096fc47d15989b0
Uzurch1k <galcenkovlad7@gmail.com>
2024-07-26 18:06:39 +0300

    fix

--- src/components/WaterProgressBar/WaterProgressBar.jsx
+++ src/components/WaterProgressBar/WaterProgressBar.jsx
@@ -1,34 +1,46 @@
+import { useRef, useEffect, useState } from 'react';
+
 import css from './WaterProgressBar.module.scss';
 
 const WaterProgressBar = () => {
-  const progressPercents = 21;
+  const progressPercents = 50;
+  const progressBarRef = useRef(null);
+  const [ellipseLeft, setEllipseLeft] = useState(0);
+
+  useEffect(() => {
+    if (progressBarRef.current) {
+      const barWidth = progressBarRef.current.offsetWidth; // Ширина родительского контейнера в пикселях
+      const percentWidth = (barWidth * progressPercents) / 100; // Ширина в пикселях для 50%
+      const leftPosition = percentWidth - 12; // Уменьшаем на 12 пикселей
+      const leftPercent = (leftPosition / barWidth) * 100; // Преобразуем в проценты
+      setEllipseLeft(leftPercent);
+    }
+  }, [progressPercents]);
 
   return (
     <div className={css.waterProgressBarSection}>
-      <h3 className={css.waterBarTitle}>today</h3>
-      <div>
-        <div className={css.waterProgressBar}>
+      <div className={css.waterProgressBarBody}>
+        <h3 className={css.waterBarTitle}>today</h3>
+
+        <div className={css.waterProgressBar} ref={progressBarRef}>
           <div
             className={css.progressBarFill}
-            style={{ width: `${progressPercents}% ` }}
+            style={{ width: `${progressPercents}%` }}
           >
-            <p
-              className={css.progressPercent}
-              style={{ left: `${progressPercents}%` }}
-            >
-              {progressPercents}%
-            </p>
             <div
-              className={css.ellipse}
-              style={{ left: `${progressPercents - 1}%` }}
-            ></div>
+              className={css.progressEllipse}
+              style={{ left: `${ellipseLeft}%` }} // Используем вычисленное значение
+            >
+              <span className={css.progressPercent}>{progressPercents}%</span>
+            </div>
           </div>
         </div>
-      </div>
-      <div className={css.percentContainer}>
-        <p className={css.waterProgressValue}>0%</p>
-        <p className={css.waterProgressValue}>50%</p>
-        <p className={css.waterProgressValue}>100%</p>
+
+        <ul className={css.percentContainer}>
+          <li className={css.waterProgressValue}>0%</li>
+          <li className={css.waterProgressValue}>50%</li>
+          <li className={css.waterProgressValue}>100%</li>
+        </ul>
       </div>
     </div>
   );
