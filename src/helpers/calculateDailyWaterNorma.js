0dbd7b794e136e600abb038d23c051d6a0af6e85
Uzurch1k <galcenkovlad7@gmail.com>
2024-07-29 20:46:34 +0300

    fix

--- src/helpers/calculateDailyWaterNorma.js
+++ src/helpers/calculateDailyWaterNorma.js
@@ -1,8 +1,8 @@
 const calculateDailyWaterNorma = (gender, userWeight, activityTime) => {
-  if (gender === 'woman') {
-    return (userWeight * 0.03 + activityTime * 0.4).toFixed(1); // Врахування часу активності в годинах
+  if (gender === 'Woman') {
+    return (userWeight * 0.03 + (activityTime * 0.4) / 60).toFixed(1);
   }
-  return (userWeight * 0.04 + activityTime * 0.6).toFixed(1); // Врахування часу активності в годинах
+  return (userWeight * 0.04 + (activityTime * 0.6) / 60).toFixed(1);
 };
 
 export default calculateDailyWaterNorma;
