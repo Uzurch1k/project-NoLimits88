c4c81b55cabef0ac8fe2d0794c8c4d5a50ef0b0e
RomanaKarasenko <143837231+RomanaKarasenko@users.noreply.github.com>
2024-07-31 01:02:16 +0300

    Add dark theme

---
+++ src/components/Providers/ThemeProvider.jsx
@@ -0,0 +1,21 @@
+import { createContext, useState, useEffect } from 'react';
+
+export const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+    const [theme, setTheme] = useState('light');
+
+    useEffect(() => {
+        if (theme === 'dark') {
+            document.body.classList.add('dark');
+        } else {
+            document.body.classList.remove('dark');
+        }
+    }, [theme]);
+
+    return (
+        <ThemeContext.Provider value={{ theme, setTheme }}>
+            {children}
+        </ThemeContext.Provider>
+    );
+};
